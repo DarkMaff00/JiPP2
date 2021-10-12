7c64e7f21763057dec00746b8e23387ce4390612
konrad.wojtowicz@interia.pl <konrad.wojtowicz@interia.pl>
2021-10-12 10:42:09 +0200

    Lab1

---
+++ allocation_basics/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc,char *argv[])
+{
+    for(int i=0;i<5;++i)        //Pętla
+    cout<< "JiPP2 " <<endl;
+
+    int rozmiar;                        //Tablica 2D
+    cout<< "Podaj rozmiar tablicy:";
+    cin>>rozmiar;
+    int **tablica = new int *[rozmiar];
+    for(int i=0;i<rozmiar;++i)
+    {
+        tablica[i]= new int [rozmiar];
+        for(int j=0;j<rozmiar;++j)
+        {
+          tablica[i][j]=i+j;
+        }
+        delete tablica[i];
+    }
+    delete tablica;
+
+    int ***tab3D = new int **[rozmiar];         //Tablica 3D
+    for(int i=0;i<rozmiar;++i)
+    {
+        tab3D[i]= new int*[rozmiar];
+    }
+    for(int j=0;j<rozmiar;++j)
+    {
+        for(int i=0;i<rozmiar;++i)
+        {
+            tab3D[i][j] = new int[rozmiar];
+            delete tab3D[i][j];
+        }
+    }
+    delete tab3D;
+
+    for(int i = 0; i < argc ; ++i) {        //Przekazanie argumentu przykład
+        cout << argv[i] <<endl;
+    }
+
+
+
+    return 0;
+
+}
